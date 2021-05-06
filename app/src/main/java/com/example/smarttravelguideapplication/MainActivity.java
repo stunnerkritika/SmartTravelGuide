8f5d7b18f2619e6723e26b7143673bdc4dcfab67
Your Name <thapakritika019@gmail.com>
2021-05-06 23:20:26 +0545

    Carousel done

--- app/src/main/java/com/example/smarttravelguideapplication/MainActivity.java
+++ app/src/main/java/com/example/smarttravelguideapplication/MainActivity.java
@@ -3,15 +3,43 @@ package com.example.smarttravelguideapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    CarouselView carouselView;
+    int[] sampleImages = {R.drawable.nepal, R.drawable.buddhist,R.drawable.stupa};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carouselView = findViewById(R.id.carouselView);
+        imageSlider();
+
+    }
+
+    private void imageSlider() {
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(sampleImages[position]);
+            }
+        });
+
+        carouselView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(MainActivity.this, sampleImages[position], Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
